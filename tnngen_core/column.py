af7ff408ab4e8411f246c761b6cce3d1b7c38ecb
Wei <125891913+TheOnlyWei24@users.noreply.github.com>
2024-03-26 16:33:25 -0400

    updated col_v according to dendrite.sv

--- tnngen_core/column.py
+++ tnngen_core/column.py
@@ -32,7 +32,149 @@ class TNN_Col():
 
 # In[ ]:
 
+    def col_active_v(self):
 
+        m = Module('column_'+str(self.p_dist)+'_'+str(self.q)+'_'+str(self.thres))
+        p_dist = m.Parameter('P_DIST', int(self.p_dist))
+        p_prox = m.Parameter('P_PROX', int(self.p_prox))
+        q = m.Parameter('Q', int(self.q))
+        wres_dist = m.Localparam('WRES_DIST', int(self.wres_dist))
+        wres_prox = m.Localparam('WRES_PROX', int(self.wres_prox))
+        thres = m.Parameter('THRESHOLD', int(self.thres))
+
+        ##################
+        # Inputs/Outputs #
+        ##################
+
+        # Input Spikes
+        input_spikes_dist = m.Input('input_spikes_dist', p_dist.value)
+        input_spikes_prox = m.Input('input_spikes_prox', p_prox.value)
+        # STDP
+        w_init_dist, capture_brv_dist, minus_brv_dist, search_brv_dist, backoff_brv_dist, min_brv_dist, F_brv_dist = [], [], [], [], [], [], []
+        w_init_prox, capture_brv_prox, minus_brv_prox, search_brv_prox, backoff_brv_prox, min_brv_prox, F_brv_prox = [], [], [], [], [], [], []
+        # STDP_dist
+        for i in range(int(q.value)):
+            for j in range(p_dist.value):
+                w_init_dist.append(m.Input('w_init_dist_'+str(i)+'_'+str(j), wres_dist.value))
+            capture_brv_dist.append(m.Input('capture_brv_dist_'+str(i), p_dist.value))
+            minus_brv_dist.append(m.Input('minus_brv_dist'+str(i), p_dist.value))
+            search_brv_dist.append(m.Input('search_brv_dist'+str(i), p_dist.value))
+            backoff_brv_dist.append(m.Input('backoff_brv_dist'+str(i), p_dist.value))
+            min_brv_dist.append(m.Input('min_brv_dist'+str(i), p_dist.value))
+            F_brv_dist.append(m.Input('F_brv_dist'+str(i), (1<<wres_dist.value)-3 + 1))
+        # STDP_prox
+        for i in range(int(q.value)):
+            for j in range(p_prox.value):
+                w_init_prox.append(m.Input('w_init_prox_'+str(i)+'_'+str(j), wres_prox.value))
+            capture_brv_prox.append(m.Input('capture_brv_prox_'+str(i), p_prox.value))
+            minus_brv_prox.append(m.Input('minus_brv_prox'+str(i), p_prox.value))
+            search_brv_prox.append(m.Input('search_brv_prox'+str(i), p_prox.value))
+            backoff_brv_prox.append(m.Input('backoff_brv_prox'+str(i), p_prox.value))
+            min_brv_prox.append(m.Input('min_brv_prox'+str(i), p_prox.value))
+            F_brv_prox.append(m.Input('F_brv_prox'+str(i), (1<<wres_prox.value)-3 + 1))
+
+        clk = m.Input('clk', 1)
+        grst = m.Input('grst', 1)
+        rstb = m.Input('rstb', 1)
+        out_spike = m.Output('output_spike', 1)
+
+        ##############
+        # Wires/Regs #
+        ##############
+
+        ein_dist = m.Wire('ein_dist', p_dist.value)
+        ein_prox = m.Wire('ein_prox', p_prox.value)
+        eout = m.Wire('eout', q.value)
+        ec_spikes = m.Wire('ec_spikes', q.value)
+        li_spikes = m.Wire('li_spikes', q.value)
+        # dist wires
+        inc_dist, dec_dist, weights_dist = [], [], []
+        for i in range(q.value):
+            inc_dist.append(m.Wire('inc_dist_'+str(i), p_dist.value))
+            dec_dist.append(m.Wire('dec_dist_'+str(i), p_dist.value))
+            for j in range(p_dist.value):
+                weights_dist.append(m.Wire('weights_dist_'+str(i)+'_'+str(j), wres_dist.value))
+        # prox wires
+        inc_prox, dec_prox, weights_prox = [], [], []
+        for i in range(q.value):
+            inc_prox.append(m.Wire('inc_prox_'+str(i), p_prox.value))
+            dec_prox.append(m.Wire('dec_prox_'+str(i), p_prox.value))
+            for j in range(p_prox.value):
+                weights_prox.append(m.Wire('weights_prox_'+str(i)+'_'+str(j), wres_prox.value))
+    
+        tnn_func = TNN_Functions()
+
+        ##################
+        # Instantiations #
+        ##################
+
+        # edge_input_gen_dist
+        pulse_dist, pulse_clk_dist = tnn_func.Pulse2edge()
+        for i in range(p_dist.value):
+            m.Instance(pulse_dist, 'pe_in_dist_'+str(i), ports = [clk, input_spikes_dist[i], grst, rstb, ein_dist[i]])
+
+        # edge_input_gen_prox
+        pulse_prox, pulse_clk_prox = tnn_func.Pulse2edge()
+        for i in range(p_prox.value):
+            m.Instance(pulse_prox, 'pe_in_prox_'+str(i), ports = [clk, input_spikes_prox[i], grst, rstb, ein_prox[i]])
+
+        # TODO: sengment
+            
+        # WTA
+        wta, _ = tnn_func.Wta(q.value)
+        m.Instance(wta, 'li', params = [q.value], ports = [ec_spikes, clk, grst, rstb, out_spike])
+            
+        # edge_output_gen
+        pulse, pulse_clk = tnn_func.Pulse2edge()
+        for i in range(q.value):
+            m.Instance(pulse, 'pe_out_'+str(i), ports = [clk, out_spike[i], grst, rstb, eout[i]])
+
+        # stdp_dist
+        stdp_dist, _ = tnn_func.Stdp(wres_dist.value)
+        for i in range(q.value):
+            for j in range(p_dist.value):
+                m.Instance(stdp_dist, 'stdp_dist_'+str(i)+'_'+str(j), params = [wres_dist.value],
+                    ports = [
+                    weights_dist[i*p_dist.value+j],
+                    ein_dist[j],
+                    eout[i],
+                    capture_brv_dist[i][j],
+                    minus_brv_dist[i][j],
+                    search_brv_dist[i][j],
+                    backoff_brv_dist[i][j],
+                    min_brv_dist[i][j],
+                    F_brv_dist[i],
+                    clk,
+                    grst,
+                    rstb,
+                    inc_dist[i][j],
+                    dec_dist[i][j]
+                    ])
+                
+        # stdp_prox
+        stdp_prox, _ = tnn_func.Stdp(wres_prox.value)
+        for i in range(q.value):
+            for j in range(p_prox.value):
+                m.Instance(stdp_prox, 'stdp_prox_'+str(i)+'_'+str(j), params = [wres_prox.value],
+                    ports = [
+                    weights_prox[i*p_prox.value+j],
+                    ein_prox[j],
+                    eout[i],
+                    capture_brv_prox[i][j],
+                    minus_brv_prox[i][j],
+                    search_brv_prox[i][j],
+                    backoff_brv_prox[i][j],
+                    min_brv_prox[i][j],
+                    F_brv_prox[i],
+                    clk,
+                    grst,
+                    rstb,
+                    inc_prox[i][j],
+                    dec_prox[i][j]
+                    ])
+
+        return m, (clk.name)
+    
     def col_v(self):
 
         m = Module('column_'+str(self.p)+'_'+str(self.q)+'_'+str(self.thres))
